a6b40c056a1a0229db754aabf2a7616c47c84d08
Xiang Wang <ramwin@qq.com>
2022-02-06 23:53:42 +0800

    doc: add README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # addr2line
 interact with addr2line command
+
+# Installation
+
+    pip install addr2line
+
+
+# Test
+
+    python -m unittest discover tests
+
+
+# usage
+
+
+        with Addr2lineContext(Path("tests/source.out")) as c:
+            self.assertEqual(
+                c.get_function(0x1189),
+                "function3",
+            )
+            self.assertEqual(
+                c.get_function(0x11a8),
+                "main",
+            )
--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,18 @@
-import pexpect
-
-# a = pexpect.spawn('addr2line', ['-f', '-e', 'source.out'])
-a = pexpect.spawn('ls')
-
-# a.expect('>')
-
-print(a.read())
-a.sendline('1189')
-print(a.read())
-# a.sendeof()
-a.sendline('1181')
+# import pexpect
+# 
+# # a = pexpect.spawn('addr2line', ['-f', '-e', 'source.out'])
+# a = pexpect.spawn('ls')
+# 
+# # a.expect('>')
+# 
+# print(a.read())
+# a.sendline('1189')
+# print(a.read())
+# # a.sendeof()
+# a.sendline('1181')
 
 # a.expect('> ')
-print(a.after)
+# print(a.after)
 
 # import subprocess
 # res = subprocess.Popen(['addr2line', '-f', '-e', 'source.out'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
