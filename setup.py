35fca89cea0722de593e113c9bc5ad18c7cc370b
Xiang Wang <ramwin@qq.com>
2022-02-07 00:04:31 +0800

    fix: set the long description content type

--- setup.py
+++ setup.py
@@ -17,10 +17,11 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     description='interact with addr2line command',
     long_description=long_description,
+    long_description_content_type="text/markdown",
 
     # The project's main homepage.
     url='https://github.com/ramwin/addr2line',
