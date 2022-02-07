9ddc848ef64ca40104107de521d00318dbe25b05
Xiang Wang <ramwin@qq.com>
2022-02-07 21:28:48 +0800

    fix: use process.stdin and process.stdout

--- addr2line/base.py
+++ addr2line/base.py
@@ -19,27 +19,33 @@ class ExecutableFileParser(object):
         self.process = None
 
     def prepare(self):
-        return
         if self.process is not None:
             raise Exception("I have been prepared")
         cmds = ["addr2line", "-f", "-e", self.executable_file]
         self.process = subprocess.Popen(cmds,
                                         stdin=subprocess.PIPE,
-                                        stdout=subprocess.PIPE)
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        text=True,
+                                        encoding="utf-8",
+                                        )
 
     def get_function(self, addr: int):
-        cmds = ["addr2line", "-f", "-e", self.executable_file, hex(addr)[2:]]
-        res = subprocess.run(cmds, encoding="utf-8", capture_output=True)
-        return res.stdout.split("\n")[0]
+        # cmds = ["addr2line", "-f", "-e", self.executable_file, hex(addr)[2:]]
+        # res = subprocess.run(cmds, encoding="utf-8", capture_output=True)
+        # return res.stdout.split("\n")[0]
         assert self.process, "I'm not prepared"
-        outs, errs = self.process.communicate(hex(addr)[2:].encode("utf-8"), timeout=1)
-        outs = outs.decode("utf-8")
-        assert errs is None
-        return outs.split("\n")[0]
+        self.process.stdin.write(hex(addr)[2:] + "\n")
+        self.process.stdin.flush()
+        function_name = self.process.stdout.readline().strip()
+        self.process.stdout.readline()
+        return function_name
 
     def stop(self):
-        return
-        self.process.kill()
+        self.process.stdin.close()
+        self.process.stdout.close()
+        self.process.stderr.close()
+        self.process.wait(1)
 
 
 class Addr2lineContext:
